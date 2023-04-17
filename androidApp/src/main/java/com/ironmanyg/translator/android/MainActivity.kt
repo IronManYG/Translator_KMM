070ba9f0d34c7dd0442874705fe0df72ab32811f
hussain.yg <hussain.yg2010@gmail.com>
2023-04-17 20:04:02 +0300

    Part3.2-BuildingTheLanguagePickers

--- androidApp/src/main/java/com/ironmanyg/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/ironmanyg/translator/android/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ironmanyg.translator.Greeting
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ironmanyg.translator.android.core.presentation.Routes
+import com.ironmanyg.translator.android.translate.presentation.AndroidTranslateViewModel
+import com.ironmanyg.translator.android.translate.presentation.TranslateScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +29,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    TranslateRoot()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun TranslateRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Routes.TRANSLATE
+    ) {
+        composable(route = Routes.TRANSLATE) {
+            val viewModel = hiltViewModel<AndroidTranslateViewModel>()
+            val state by viewModel.state.collectAsState()
+            TranslateScreen(
+                state = state,
+                onEvent = viewModel::onEvent
+            )
         }
     }
 }
---
+++ androidApp/src/main/java/com/ironmanyg/translator/android/di/AppModule.kt
@@ -0,0 +1,56 @@
+package com.ironmanyg.translator.android.di
+
+import android.app.Application
+import com.ironmanyg.translator.database.TranslateDatabase
+import com.ironmanyg.translator.translate.data.history.SqlDelightHistoryDataSource
+import com.ironmanyg.translator.translate.data.local.DatabaseDriverFactory
+import com.ironmanyg.translator.translate.data.remote.HttpClientFactory
+import com.ironmanyg.translator.translate.data.translate.KtorTranslateClient
+import com.ironmanyg.translator.translate.domain.history.HistoryDataSource
+import com.ironmanyg.translator.translate.domain.translate.Translate
+import com.ironmanyg.translator.translate.domain.translate.TranslateClient
+import com.squareup.sqldelight.db.SqlDriver
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient {
+        return HttpClientFactory().create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTranslateClient(httpClient: HttpClient): TranslateClient {
+        return KtorTranslateClient(httpClient)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabaseDriver(app: Application): SqlDriver {
+        return DatabaseDriverFactory(app).create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHistoryDataSource(driver: SqlDriver): HistoryDataSource {
+        return SqlDelightHistoryDataSource(TranslateDatabase(driver))
+    }
+
+    @Provides
+    @Singleton
+    fun provideTranslateUseCase(
+        client: TranslateClient,
+        dataSource: HistoryDataSource
+    ): Translate {
+        return Translate(client, dataSource)
+    }
+}
