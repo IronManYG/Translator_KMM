516f5df96677a8796e4d4164e9fa6014c2652886
hussain.yg <hussain.yg2010@gmail.com>
2023-04-25 20:57:44 +0300

    PartPart4.5-TextToSpeech

---
+++ iosApp/iosApp/translate/presentation/TextToSpeech.swift
@@ -0,0 +1,22 @@
+//
+//  TextToSpeech.swift
+//  iosApp
+//
+//  Created by Hussain Gaddal on 25/04/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+struct TextToSpeech {
+    
+    private let synthesizer = AVSpeechSynthesizer()
+    
+    func speak(text: String, language: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: language)
+        utterance.volume = 1
+        synthesizer.speak(utterance)
+    }
+}
