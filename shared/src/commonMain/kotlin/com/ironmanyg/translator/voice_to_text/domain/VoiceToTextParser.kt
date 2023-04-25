2ebfc2352600319fd748f5eff5af96ed0a46d019
hussain.yg <hussain.yg2010@gmail.com>
2023-04-25 23:25:00 +0300

    Part5.1-AndroidVoiceToTextParser

---
+++ shared/src/commonMain/kotlin/com/ironmanyg/translator/voice_to_text/domain/VoiceToTextParser.kt
@@ -0,0 +1,11 @@
+package com.ironmanyg.translator.voice_to_text.domain
+
+import com.ironmanyg.translator.core.domain.util.CommonStateFlow
+
+interface VoiceToTextParser {
+    val state: CommonStateFlow<VoiceToTextParserState>
+    fun startListening(languageCode: String)
+    fun stopListening()
+    fun cancel()
+    fun reset()
+}
